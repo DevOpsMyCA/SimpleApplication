44759d3872e3dcd5f8acb8928a3dbc218558b6a4
x16134672 <x16134672@student.ncirl.ie>
2025-03-12 01:40:11 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,12 @@
-#!/bin/bas
- sudo apt updateudo apt install -y curl
- #!/bin/bash
- sudo apt update
- sudo apt install -y curl
- curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
- sudo apt install -y nodejs
- node -v
- npm -v
- sudo npm install -g pm2
- pm2 stop example_app || true
- cd SimpleApplication
- npm install
- pm2 start ./bin/www --name example_appcurl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
- sudo apt install -y nodejs
- node -v
- npm -v
- sudo npm install -g pm2
- pm2 stop example_app || true
- cd SimpleApplication
- npm install
- pm2 start ./bin/www --name example_app
+#!/bin/bash
+sudo apt update
+sudo apt install -y curl
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
+node -v
+npm -v
+sudo npm install -g pm2
+pm2 stop example_app || true
+cd /home/ubuntu/SimpleApplication
+npm install
+pm2 start ./bin/www --name example_app
